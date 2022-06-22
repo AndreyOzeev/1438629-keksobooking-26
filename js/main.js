64fc928470ffdc5409c34c09b63010723bb30fd4
Andrey Ozeev <andrey.ozeev@mail.ru>
2022-06-22 22:50:36 +0300

    добавил метод абсолюнтого числа

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Получение рандомного числа из диапазона
 
 function getRandomPositiveInteger(a, b) {
-  const lesser = Math.ceil(Math.min((a), (b)));
-  const more = Math.floor(Math.max((a), (b)));
+  const lesser = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const more = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (more - lesser + 1) + lesser;
   return Math.floor(result);
 }
@@ -10,10 +10,10 @@ getRandomPositiveInteger(1, 1000);
 
 //Получение рандомного числа с плавающей точкой до десяток
 
-function getRandomPositiveFloat(a, b) {
-  const lesser = Math.min((a), (b));
-  const more = Math.max((a), (b));
-  const result = Math.random() * (more - lesser + 1) + lesser;
+function getRandomPositiveFloat(a, b, digit) {
+  const lesser = Math.min(Math.abs(a), Math.abs(b));
+  const more = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (more - lesser + digit) + lesser;
   return +result.toFixed(1);
 }
 
